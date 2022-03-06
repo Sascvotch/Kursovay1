b5f829fc90d5cebc7a4029c2311319dae851bf49
Sascvotch <sascvotch@yandex.ru>
2022-03-06 15:10:52 +0300

    Курсовая базовая

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,89 @@
+package ru.skypro;
+
+import java.util.Objects;
+
+public class Employee {
+   static int id;
+    String name;
+    String lastName;
+    String middleName;
+    String department;
+    double salary;
+
+    public Employee( String name, String lastName, String middleName, String department, double salary) {
+        id = id++;
+        this.name = name;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+   public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
